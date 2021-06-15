d3fc3d8da65327e5c8e71d48c31e09be7a8103a0
rafaelasencioB <rafaelasenciodeveloper@gmail.com>
2021-06-15 10:36:22 +0200

    Replace optional params with enum Result

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -66,14 +66,14 @@ class RemoteFeedLoaderTests: XCTestCase {
         var requestedURLs: [URL] {
             return messages.map { $0.url }
         }
-        var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
+        var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
         
-        func get(from url: URL, completion: @escaping(Error?, HTTPURLResponse?) -> Void){
+        func get(from url: URL, completion: @escaping(HTTPClientResult) -> Void){
             messages.append((url, completion))
         }
         
         func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(error, nil)
+            messages[index].completion(.failure(error))
         }
         
         func complete(withStatusCode code: Int, at index: Int = 0) {
@@ -82,8 +82,8 @@ class RemoteFeedLoaderTests: XCTestCase {
                 url: requestedURLs[index],
                 statusCode: code,
                 httpVersion: nil,
-                headerFields: nil)
-            messages[index].completion(nil, response)
+                headerFields: nil)!
+            messages[index].completion(.sucess(response))
         }
     }
 }
