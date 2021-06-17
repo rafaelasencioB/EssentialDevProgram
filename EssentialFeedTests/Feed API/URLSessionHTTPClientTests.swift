fd95a520fe2e791a97ff5b6a03728bc0a2f1b246
rafaelasencioB <rafaelasenciodeveloper@gmail.com>
2021-06-17 12:07:40 +0200

    Creates URLSession data task with URL on `URLSessionHTTPClient.get(from: URL)`

---
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -0,0 +1,47 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  EssentialFeedTests
+//
+//  Created by RafaelAsencio on 17/06/2021.
+//
+
+import XCTest
+
+class URLSessionHTTPClient {
+    private let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func get(from url: URL) {
+        session.dataTask(with: url) { (_, _, _) in
+        }
+    }
+}
+
+class URLSessionHTTPClientTests: XCTestCase {
+    
+    func test_getFromURL_createsDataTaskWithURL() {
+        let url = URL(string: "any-URL.com")!
+        let session = URLSessionSpy()
+        let sut = URLSessionHTTPClient(session: session)
+        sut.get(from: url)
+        XCTAssertEqual(session.receivedURLs, [url])
+    }
+    
+    //MARK: - Helpers
+    
+    class URLSessionSpy: URLSession {
+        var receivedURLs = [URL]()
+        
+        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+            receivedURLs.append(url)
+            return FakeURLSessionDataTask()
+        }
+        
+        class FakeURLSessionDataTask: URLSessionDataTask {}
+    }
+}
+
+
