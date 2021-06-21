a40cc6f9619e65983b5cac95e2bc4c2d92c45506
rafael.asencio <rafaelasenciodeveloper@gmail.com>
2021-06-21 16:18:47 +0200

    Move `URLSessionHTTPClient` to its own file in production

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -16,32 +16,6 @@ import EssentialFeed
 //    func resume()
 //}
 
-class URLSessionHTTPClient: HTTPClient {
-    private let session: URLSession
-    
-    init(session: URLSession = .shared) {
-        self.session = session
-    }
-    
-    struct UnexpectedValuesRepresentation: Error {
-        
-    }
-    
-    func get(from url: URL, completion: @escaping(HTTPClientResult) -> Void) {
-        session.dataTask(with: url) { (data, response, error) in
-            if let error = error {
-                completion(.failure(error))
-            }
-            else if let data = data, let response = response as? HTTPURLResponse {
-                completion(.sucess(data, response))
-            }
-            else {
-                completion(.failure(UnexpectedValuesRepresentation()))
-            }
-        }.resume()
-    }
-}
-
 class URLSessionHTTPClientTests: XCTestCase {
     override func setUp() {
         URLProtocolStub.startInterceptingRequests()
